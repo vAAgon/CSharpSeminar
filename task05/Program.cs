3fdbb9b29be489b3d269368f40c0cb8fad644d61
Maxim <maximkhlopunov@gmail.com>
2023-03-24 21:12:58 +0600

    task05

---
+++ task05/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.Clear();
+Console.WriteLine("Введите число");
+ string num = Console.ReadLine();
+ if (num != null)
+ {
+int lastnum = int.Parse(num);
+int  firstnum = lastnum * -1; 
+string res = string.Empty;
+while(firstnum <=lastnum)
+{
+    res = res + firstnum + " ";
+    firstnum ++ ;
+}
+Console.WriteLine(res);
+ }
