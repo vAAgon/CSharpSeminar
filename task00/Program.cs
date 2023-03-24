a792e4c183310f60cb47e1137022bc2dd47aaac7
Maxim <maximkhlopunov@gmail.com>
2023-03-24 19:14:57 +0600

    maketask1

--- task00/Program.cs
+++ task00/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+ string inLine  = Console.ReadLine();
+ if (inLine!=null)
+ {
+    int number1 = int.Parse(inLine);
+ 
+Console.WriteLine(number1*number1);
+ }
