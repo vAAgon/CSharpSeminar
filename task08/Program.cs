7265d09c0111012b877c33db4c3739d53a652bd3
Maxim <maximkhlopunov@gmail.com>
2023-03-27 22:44:27 +0600

    task08

---
+++ task08/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.Write("Введите чесло :");
+int num = Convert.ToInt32(Console.ReadLine());
+int extra = 2;
+
+if (num > 1)
+{
+    while (extra <= num)
+    {
+        Console.WriteLine(extra + " ");
+        extra = extra + 2;  
+    }
+
+}
+
+
+
+
+
+
