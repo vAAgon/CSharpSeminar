ad2eb0de3977bfd18e3d410f61783246322a5cc6
Maxim <maximkhlopunov@gmail.com>
2023-03-30 00:24:04 +0600

    task01.1

--- task00/Program.cs
+++ task00/Program.cs
@@ -1,8 +1,12 @@
-﻿Console.Clear();
- string inLine  = Console.ReadLine();
- if (inLine!=null)
- {
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+
+Console.Clear();
+Console.Write("input num. :");
+string? inLine = Console.ReadLine();
+if (inLine != null)
+{
     int number1 = int.Parse(inLine);
- 
-Console.WriteLine(number1*number1);
- }
+    int numberMath = (int)Math.Pow(number1,2);
+    Console.WriteLine(number1 * number1);
+    Console.WriteLine(numberMath);
+}
