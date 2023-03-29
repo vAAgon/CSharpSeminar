8acd9e9a3c25060f958d594766b6c6e47d01543c
Maxim <maximkhlopunov@gmail.com>
2023-03-30 00:25:39 +0600

    task112

---
+++ task112/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.WriteLine("Введите чесло 1 ");
+double num1 = double.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите чесло 2 ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+bool res = (num2 % num1 == 0) ? (Console.WriteLine($"число{num1} кратно{num2}")) : (Console.WriteLine("Остаток{num%num1} "));
