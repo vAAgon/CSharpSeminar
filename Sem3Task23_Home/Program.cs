30870327456279d234e458199d0bd3e132a12342
Maxim <maximkhlopunov@gmail.com>
2023-04-02 20:40:55 +0600

    add task 23

--- Sem3Task23_Home/Program.cs
+++ Sem3Task23_Home/Program.cs
@@ -1,13 +1,13 @@
 ﻿Console.Clear();
 //Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-int ReadData(string massage)
+int ReadData(string massage) // Метод ввода 
 {
     Console.Write(massage);
    int res = Convert.ToInt32(Console.ReadLine());
     return res;
 }
 
-string LineGen(int num, int pow)
+string LineGen(int num, int pow) // Метод ввывода чисел 
 {
     string res = "";
     for (int i = 1; i <= num; i++)
@@ -16,7 +16,7 @@ string LineGen(int num, int pow)
     }
     return res ;
 }
-
+// Ввывод в кансоль 
 int numN = ReadData("Input num :");
 Console.WriteLine(LineGen(numN, 1));
 Console.WriteLine(LineGen(numN, 3));
